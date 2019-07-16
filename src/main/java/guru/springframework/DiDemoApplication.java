9c966ad830f6d6e0f9865b3d8701fbcc46645c8c
Ana Ilie <ana1.ilie@orange.com>
2019-07-16 15:18:59 +0300

    all in 1

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -20,5 +20,10 @@ public class DiDemoApplication {
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+		//commit last before push
+		//commit 400 before merging
 	}
+	
+	//commit merge
 }
